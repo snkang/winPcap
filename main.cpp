3b6008bdc6759b9e4676b87275e1ff5e222af2e7
snkang <victor1101@naver.com>
2016-11-15 10:43:39 -0500

    sample1

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <QCoreApplication>
+
+#define WPCAP
+#define HAVE_REMOTE
+#include <pcap.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    pcap_if_t *alldevs;
+    pcap_if_t *d;
+    int i=0;
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    /* Retrieve the device list from the local machine */
+    if (pcap_findalldevs(&alldevs, errbuf) == -1)
+    {
+        fprintf(stderr,"Error in pcap_findalldevs_ex: %s\n", errbuf);
+        exit(1);
+    }
+
+    /* Print the list */
+    for(d= alldevs; d != NULL; d= d->next)
+    {
+        printf("%d. %s", ++i, d->name);
+        if (d->description)
+            printf(" (%s)\n", d->description);
+        else
+            printf(" (No description available)\n");
+    }
+
+    if (i == 0)
+    {
+        printf("\nNo interfaces found! Make sure WinPcap is installed.\n");
+        return 0;
+    }
+
+    /* We don't need any more the device list. Free it */
+    pcap_freealldevs(alldevs);
+
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}
